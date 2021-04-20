d164e1a76d9af35f8d82ac552e120d36e1111ea4
Ariel Rodriguez <arielseba.rodriguez@gmail.com>
2021-04-20 13:08:59 -0300

    nuevo commit

--- proyectoJavascript.js
+++ proyectoJavascript.js
@@ -1,12 +1,15 @@
-//Declaramos la url del archivo JSON local
-const URLJSON = "productos.json"
-
-
-
+//FUNCION QUE MUESTRA UN ALERT PERSONALIZADO AL AGREGAR UN PRODUCTO AL CARRO
+function mostrar(){
+    swal("Muchas gracias","El producto fue agregado exitosamente","success") ;
+}
+//ANIMACION DEL LOGO DE LA EMPRESA
 $(".logo").animate({
     width: '150px',
     opacity: '1'},"slow", );
 
+//Declaramos la url del archivo JSON local
+const URLJSON = "productos.json"
+//INTRODUCIMOS LOS PRODUCTOS AL DIV A TRAVES DEL ARCHIVO JSON
 $.getJSON(URLJSON, function (respuesta, estado) {
     if(estado === "success"){
       let misDatos = respuesta;
@@ -15,10 +18,9 @@ $.getJSON(URLJSON, function (respuesta, estado) {
                                 <img  class="imgcard" src="${dato.img}" alt="">
                                 <strong><p >${dato.descripcion} </p>
                                 <p>$ ${dato.precio}</p></strong>
-                                <button id="${dato.id}" style="height: auto; margin-bottom: 10px;" class="btn btn-success">Agregar</button>
+                                <button id="${dato.id}" style="height: auto; margin-bottom: 10px;" class="btn btn-warning">Agregar</button>
                                 </div>`)  }
-let preguntarComprar="";
-let nombre="";
+//DECLARACION DE VARIABLES
 let totalImporte=0;
 let totalDescripciones="";
 let contadorCarrito=0;
@@ -46,45 +48,39 @@ class carrito{
     }
 
 //Declaracion de las gorras lisas
-let gorraNegra=new gorrasLisas ( 1, "Gorra Lisa Negra" ,  2000,  0); 
-let gorraBeige=new gorrasLisas ( 2, "Gorra Lisa Beige" ,  1500,  0); 
-let gorraAzulM=new gorrasLisas ( 3,"Gorra Lisa Azul Marino" ,  2000,0); 
-let gorraAzulC=new gorrasLisas ( 4,"Gorra Lisa Azul Clasico" ,  1500,0); 
-let gorraBlanca=new gorrasLisas ( 5,"Gorra Lisa Blanca Clasica" ,  1500,0); 
-//let gorrasLisasA=[gorraNegra,gorraBeige,gorraAzulM,gorraAzulC,gorraBlanca];
+let gorraNegra=new gorrasLisas ( 1, "Gorra Lisa Negra" ,  14.99,  0); 
+let gorraBeige=new gorrasLisas ( 2, "Gorra Lisa Beige" ,  15.99,  0); 
+let gorraAzulM=new gorrasLisas ( 3,"Gorra Lisa Azul Marino" ,  14.99,0); 
+let gorraAzulC=new gorrasLisas ( 4,"Gorra Lisa Azul Clasico" ,  10.99,0); 
+let gorraBlanca=new gorrasLisas ( 5,"Gorra Lisa Blanca Clasica" ,  14.99,0); 
 //Declaracion de las gorras de Disney
-let gorraMickeyRosa=new gorrasLisas ( 6, "Gorra Mickey Rosa" ,  2500,  0); 
-let gorraMickeyNegra=new gorrasLisas (7, "Gorra Mickey Negra" ,  2500, 0);
-let gorraMinnieNegra=new gorrasLisas ( 8,"Gorra Minnie Negra" ,  2000,  0);
-let gorraDonald=new gorrasLisas   (9, "Gorra Pato Donald Combinada" ,  2300,0);
-let gorraMickeyRoja=new gorrasLisas (10, "Gorra Mickey Roja" ,  3000,  0); 
-let gorraPluto=new gorrasLisas (11, "Gorra Pluto Clasica" ,  2000,  0); 
-//let gorrasDisneyA=[gorraMickeyRosa,gorraMickeyNegra,gorraMinnieNegra,gorraDonald,gorraMickeyRoja,gorraPluto];
+let gorraMickeyRosa=new gorrasLisas ( 6, "Gorra Mickey Rosa" ,  18.99,  0); 
+let gorraMickeyNegra=new gorrasLisas (7, "Gorra Mickey Negra" ,  18.99, 0);
+let gorraMinnieNegra=new gorrasLisas ( 8,"Gorra Minnie Negra" ,  16.99,  0);
+let gorraDonald=new gorrasLisas   (9, "Gorra Pato Donald Combinada" ,  18.99,0);
+let gorraMickeyRoja=new gorrasLisas (10, "Gorra Mickey Roja" ,  19.99,  0); 
+let gorraPluto=new gorrasLisas (11, "Gorra Pluto Clasica" ,  15.99,  0); 
 //Declaracion de las gorras de los supercampeones
-let gorraAzul=new gorrasLisas ( 12,"Gorra Super Campeones Azul" ,  1200,  0); 
-let gorraAzulR=new gorrasLisas ( 13,"Gorra Super Campeones Azul c/Rojo" ,  1200, 0); 
-let gorraAzulB=new gorrasLisas ( 14,"Gorra Super Campeones Azul c/Blanco" ,  1200, 0); 
-let gorraAzulRo=new gorrasLisas ( 15,"Gorra Super Campeones Azul c/Rojo" ,  1200,  0); 
-let gorraNegraC=new gorrasLisas ( 16,"Gorra Super Campeones Negra" ,  1200,  0); 
-let gorraNegraE=new gorrasLisas ( 17,"Gorra Super Campeones Negra Estampada" ,  1500,  0); 
-let gorraNegraD=new gorrasLisas ( 18,"Gorra Super Campeones Negra c/Dorada" ,  1500,  0); 
-//let gorraSuperCampeonesA=[gorraAzul,gorraAzulR,gorraAzulB,gorraAzulRo,gorraNegraC,gorraNegraE,gorraNegraD];
-
+let gorraAzul=new gorrasLisas ( 12,"Gorra Super Campeones Azul" ,  10.99,  0); 
+let gorraAzulR=new gorrasLisas ( 13,"Gorra Super Campeones Azul c/Rojo" ,  10.99, 0); 
+let gorraAzulB=new gorrasLisas ( 14,"Gorra Super Campeones Azul c/Blanco" ,  11.99, 0); 
+let gorraAzulRo=new gorrasLisas ( 15,"Gorra Super Campeones Azul c/Rojo" ,  10.99,  0); 
+let gorraNegraC=new gorrasLisas ( 16,"Gorra Super Campeones Negra" ,  12.99,  0); 
+let gorraNegraE=new gorrasLisas ( 17,"Gorra Super Campeones Negra Estampada" ,  14.99,  0); 
+let gorraNegraD=new gorrasLisas ( 18,"Gorra Super Campeones Negra c/Dorada" ,  14.99,  0); 
 //Declaracion de las gorras de Futbol
-let gorraIndependiente=new gorrasLisas (19, "Gorra Independiente " ,  1600,  0); 
-let gorraBoca=new  gorrasLisas( 20,"Gorra Boca " ,  1600,  0); 
-let gorraRiver=new gorrasLisas ( 21,"Gorra River " ,  2000, 0); 
-let gorraRacing=new gorrasLisas ( 22,"Gorra Racing " ,  1600,0);
-//let gorrasFutbolA=[gorraIndependiente,gorraBoca,gorraRiver,gorraRacing]; 
-
+let gorraIndependiente=new gorrasLisas (19, "Gorra Independiente " ,  9.99,  0); 
+let gorraBoca=new  gorrasLisas( 20,"Gorra Boca " ,  4.99,  0); 
+let gorraRiver=new gorrasLisas ( 21,"Gorra River " ,  20.99, 0); 
+let gorraRacing=new gorrasLisas ( 22,"Gorra Racing " ,  10.99,0);
+//DECLARACION DE ARRAY CON TODOS LOS PRODUCTOS
 let totalProductos=[gorraNegra,gorraBeige,gorraAzulM,gorraAzulC,gorraBlanca,gorraMickeyRosa,gorraMickeyNegra,gorraMinnieNegra,gorraDonald,gorraMickeyRoja,gorraPluto,
  gorraAzul,gorraAzulR,gorraAzulB,gorraAzulRo,gorraNegraC,gorraNegraE,gorraNegraD,gorraIndependiente,gorraBoca,gorraRiver,gorraRacing];
  
 
- 
+//SE AGREGAN EVENTOS AL HACER CLICK 
 let agregar=document.getElementById("1");
 agregar.addEventListener("click",()=>{productoAgregado(1)});
-    
 agregar=document.getElementById("2");
 agregar.addEventListener("click",()=>{productoAgregado(2)});
 agregar=document.getElementById("3");
@@ -128,53 +124,40 @@ agregar.addEventListener("click",()=>{productoAgregado(21)});
 agregar=document.getElementById("22");
 agregar.addEventListener("click",()=>{productoAgregado(22)});
 
-
-
-
+//FUNCION QUE AGREGA PRODUCTOS AL CARRITO DE COMPRAS
 function productoAgregado(id){
-  
+    //SI EL LOCAL STORAGE NO ESTA VACIO, IGUALO EL CARRITO CON EL CONTENIDO DE LOCAL STORAGE
     if (localStorage.carritoA!=undefined) {
-        carritoA=JSON.parse(localStorage.carritoA);
-        console.log("La pagina anterior trae",carritoA);
-        let verificador=0;      
+       carritoA=JSON.parse(localStorage.carritoA);
+       let verificador=0;      
+        //RECORRO LOS ELEMENTOS DEL CARRITO, SI SE REPITE UN ID LE SUMO 1 UNIDAD, SINO PONGO EL VALOR DE VERIFICADOR EN 1(FLAG)
         for(const elem of carritoA){
-                
-            if(elem.id==id){
-            console.log("Entro al if dentro del for")
-            console.log("Recorro lo que hay en el carrito y sumo 1 unidad en caso de que se encuentre el id");
-            elem.cantidadG=elem.cantidadG+1;
-            verificador=1;
-            console.log("El verificador vale", verificador); 
+                if(elem.id==id){
+                elem.cantidadG=elem.cantidadG+1;
+                verificador=1;
+                }
+            
+                if(verificador!=1){ 
+                        verificador=0;
+                    }
             }
-            console.log("El verificador vale", verificador); 
-            if(verificador!=1){ 
-                    verificador=0;
-                console.log("El verificador deberia entrar a 0(else)", verificador); 
-            }
-        }
-        console.log("El verificador vale : " ,verificador);
-        if(verificador===0  ){
-            console.log("Termine de recorrer el carrito y sumo 1 unidad y pusheo el producto en el caso de que no se encuentre el id");
-            totalProductos[id-1].cantidadG=totalProductos[id-1].cantidadG+1;
-            carritoA.push(totalProductos[id-1]);  
-              }
-            console.log(localStorage.carritoA);
+        //EN CASO DE NO ENCONTRARSE EL ID, SUMO 1 UNIDAD Y PUSHEO EL PRODUCTO AL CARRITO
+            if(verificador===0  ){
+                totalProductos[id-1].cantidadG=totalProductos[id-1].cantidadG+1;
+                carritoA.push(totalProductos[id-1]);  
+                }
+            
     }
-  
+  //SI EL LOCAL STORAGE ESTA VACIO, SUMO 1 UNIDAD AL PRODUCTO Y PUSHEO EL PRODUCTO AL CARRITO. LUEGO MUESTO EL ALERT PERSONALIZADO CON LA FUNCION MOSTRAR Y GUARDO EN LOCAL STORAGE EL CONTENIDO DE CARRITO.
     if (localStorage.carritoA==undefined ){
      console.log("No hay nada en el carrito (undefined) pusheo el producto seleccionado");
      totalProductos[id-1].cantidadG=totalProductos[id-1].cantidadG+1;
-     carritoA.push(totalProductos[id-1]);    
-     console.log("CarritoA",carritoA);}
-    
-     
-    alert("El producto fue agregado exitosamente");
-    localStorage.setItem("carritoA",JSON.stringify(carritoA));
-    console.log("---------------------------------------------------------");
-}
-
-
-    }});
+     carritoA.push(totalProductos[id-1]);    }
+     mostrar();
+     localStorage.setItem("carritoA",JSON.stringify(carritoA));
+    }
+ }
+});
 
     
 
--- proyectoJavascriptChango.js
+++ proyectoJavascriptChango.js
@@ -1,33 +1,67 @@
 let carritoA=JSON.parse(localStorage.getItem("carritoA"));
-console.log("que nos trae da la pagina anterior" , carritoA)
+
 total=0;
 
+//OBTENGO LA COTIZACION DEL DOLAR A TRAVES DE UNA API
+let dolar=0;
+let url = "https://mercados.ambito.com/dolar/oficial/variacion";
+const api=new XMLHttpRequest();
+api.open("GET",url,true);
+api.send();
+api.onreadystatechange=function(){
+  if(this.status == 200 && this.readyState==4){
+  let datos=JSON.parse(this.responseText);
+
+ dolar=parseInt(datos.compra);
+ console.log("valor dolar adentro",dolar)
+  
+
+  }
+}
+$(document).ready(function(){
+
+    
+  
 $("#jqueryCarrito").fadeIn(3000);
 $("#jqueryCarrito").slideDown("fast");
 
 $(".logo").animate({
-    width: '150px',
+    width: 'auto',
     opacity: '1'},"slow");
+    
 
-/*$(document).ready(function(){
-    console.log("EL DOM ESTA LISTO");
-});*/
+
+if(carritoA==undefined){
+$("#carroVacio").append(`<div>
+<h3 style="color:white ;text-align: center;">Por el momento usted no ha seleccionado ningun producto de nuestra tienda </h3>                                
+</div>`)}
+
+if(carritoA!=undefined){
+    $("#tabla").append(`<tr style="color:yellow; font-size:150%; "> <td>${"id"}</td>
+                             <td>${"Producto"}</td>
+                             <td>${"Importe"}</td>
+                             <td></td>
+                             <td>${"Cantidad"}</td>
+                             <td></td>
+                             <td>${"Importe Total"}</td>
+                             <td>${"Eliminar"}</td>
+                        
+                          `)
 
             for(const elem of carritoA){
            
-                $("#jqueryCarrito").append(`<tr id="producto${elem.id}" style="font-weight:700"><td id="${elem.id} "> ${elem.id}  </td>
+                $("#tabla").append(`<tr style="color:white;margin-top:20%" id="producto${elem.id}" style="font-weight:700">
+                                <td id="${elem.id} "> ${elem.id}  </td>
                                 <td> ${elem.descripcion}  </td>
                                 <td style="width: 20%"> $ ${elem.precio} </td>
-                                <td><button  class = "btn-xs btn-success " id="botonAgregar${elem.id}"> + </button></td>
-                                <td id="CantidadG${elem.id}"> ${elem.cantidadG}</td>
-                                <td> <button class = "btn-xs btn-warning" id="botonQuitar${elem.id}"> - </button></td>
+                                <td><button style=" padding:1%; margin-right:15%"; class = "btn-xs btn-success " id="botonAgregar${elem.id}"> + </button></td>
+                                <td id="CantidadG${elem.id}"> ${elem.cantidadG} </td>
+                                <td><button style=" padding:   1%; margin-left:15%" class = "btn-xs btn-warning" id="botonQuitar${elem.id}"> - </button></td>
                                 <td id="CantidadT${elem.id}"> $ ${elem.precio * elem.cantidadG} </td>
-                               
-                               
                                 <td><button class = "btn-xs btn-danger" id="botonEliminar${elem.id}"> x </button>   </td>
                                 ${total=total+(elem.cantidadG*elem.precio)}
-                               <tr>
-                                <hr> 
+                               
+                                
                                 `);
 
                 
@@ -40,8 +74,9 @@ $(".logo").animate({
                                    $(`#CantidadG${elem.id}`).html(elem.cantidadG);
                                    $(`#CantidadT${elem.id}`).html("$ "+ elem.cantidadG*elem.precio);
                                    total=total+elem.precio;
-                                   $(`#totalImporteChango`).html(` <h3 style="color:green ;text-align: center;">   <button class = "btn btn-secondary" id="vaciarCarrito" style="margin-right:40%">VaciarCarrito</button> Total Importe Carrito       $ ${total} </h3>                                
-                                   </div>`);
+                                   $(`#totalImporteChango`).html(`<div style="margin-left:20%; display: flex; justify-content: space-between;">
+                                   <button class="btn btn-secondary" id="vaciarCarrito">    VaciarCarrito  </button><h3 style="color:white"; font-size:100px">Total Compra  $ ${total}</h3><button class = "btn btn-success" id="finalizarCompra" style="margin-right:40%">Finalizar Compra</button> 
+                                    </div>`) 
                                    localStorage.setItem("carritoA",JSON.stringify(carritoA));
                                     
 
@@ -56,8 +91,9 @@ $(".logo").animate({
                                     $(`#CantidadG${elem.id}`).html(elem.cantidadG);
                                     $(`#CantidadT${elem.id}`).html("$ "+ elem.cantidadG*elem.precio);
                                     total=total-elem.precio;
-                                    $(`#totalImporteChango`).html(` <h3 style="color:green ;text-align: center;">   <button class = "btn btn-secondary" id="vaciarCarrito" style="margin-right:40%">VaciarCarrito</button> Total Importe Carrito       $ ${total} </h3>                                
-                                   </div>`);
+                                    $(`#totalImporteChango`).html(`<div style="margin-left:20%; display: flex; justify-content: space-between;">
+                                    <button class="btn btn-secondary" id="vaciarCarrito">    VaciarCarrito  </button><h3 style="color:white"; font-size:100px">Total Compra  $ ${total}</h3><button class = "btn btn-success" id="finalizarCompra" style="margin-right:40%">Finalizar Compra</button> 
+                                     </div>`) 
                                     localStorage.setItem("carritoA",JSON.stringify(carritoA)); 
                                    
                                     }
@@ -71,11 +107,18 @@ $(".logo").animate({
                                     $(`#producto${elem.id}`).remove();
                                     carritoA.splice(busquedaId,1);   
                                     total=total-(elem.precio*elem.cantidadG);                                
-                                    $(`#totalImporteChango`).html(` <h3 style="color:green ;text-align: center;">   <button class = "btn btn-secondary" id="vaciarCarrito" style="margin-right:40%">VaciarCarrito</button> Total Importe Carrito       $ ${total} </h3>                                
-                                    </div>`);  
-
+                                    $(`#totalImporteChango`).html(`<div style="margin-left:20%; display: flex; justify-content: space-between;">
+                                    <button class="btn btn-secondary" id="vaciarCarrito">    VaciarCarrito  </button><h3 style="color:white"; font-size:100px">Total Compra  $ ${total}</h3><button class = "btn btn-success" id="finalizarCompra" style="margin-right:40%">Finalizar Compra</button> 
+                                     </div>`) 
+                                    if(total===0){
+                                        $(`#totalImporteChango`).remove();
+                                        $("#carroVacio").append(`<div>
+                                        <h3 style="color:white ;text-align: center;">Por el momento usted no ha seleccionado ningun producto de nuestra tienda </h3>                                
+                                        </div>`)}
                                     localStorage.setItem("carritoA",JSON.stringify(carritoA)); 
+                                   console.log(carritoA);
                                 });
+                                
                                     }
                                      
                               )
@@ -85,22 +128,100 @@ $(".logo").animate({
 
  
                 }
-
             
+                $("#totalImporteChango").append(`<div style="margin-left:20%; display: flex; justify-content: space-between;">
+                               <button class="btn btn-secondary" id="vaciarCarrito">    VaciarCarrito  </button><h3 style="color:white"; font-size:100px">Total Compra  $ ${total}</h3><button class = "btn btn-success" id="finalizarCompra" style="margin-right:40%">Finalizar Compra</button> 
+                                </div>`)  
+               }
+           
+         
+         
                 
-                if(carritoA!=undefined){
-                    $("#totalImporteChango").append(`<div>
-                               <h3 style="color:green ;text-align: center;">   <button class = "btn btn-secondary" id="vaciarCarrito" style="margin-right:40%">VaciarCarrito</button> Total Importe Carrito       $ ${total} </h3>                                
-                    </div>`)    }
+                
                
+                
                 $("#vaciarCarrito").on("click", function(){    
-                    $("#jqueryCarrito").slideUp("slow",function(){    
+                   
+                    $("#tabla").slideUp("slow",function(){    
                     
-                    $(`#jqueryCarrito`).remove();  
+                    $(`#tabla`).remove();  
+                    $(`#totalImporteChango`).remove();
+                    $(`#tabla   `).remove();
+                    $("#carroVacio").append(`<div>
+                    <h3 style="color:white ;text-align: center;">Por el momento usted no ha seleccionado ningun producto de nuestra tienda </h3>                                
+                    </div>`)    
                     total=0;                           
                     $(`#totalImporteChango`).html(` <h3 style="color:green ;text-align: center;">   <button class = "btn btn-secondary" id="vaciarCarrito" style="margin-right:40%">VaciarCarrito</button> Total Importe Carrito       $ ${total} </h3>                                
                     </div>`);   
                                       
                     window.localStorage.removeItem("carritoA");});  
+                
                 });
-             
+    
+                $("#finalizarCompra").on("click", function(){    
+                  
+                            
+                  swal("Importe a abonar","En ARS: \t\t\t\t\t\t$" + (total*dolar).toFixed(2) + "\n" + "En USD: $" + total,"success" ) ;
+
+                    swal("Muchas gracias por su compra","A continuacion le pediremos algunos datos para concluir la compra.","success") ;
+
+               
+                    
+                    $("#tabla").fadeOut("5000",function(){    
+                    
+                        })
+                        $("#totalImporteChango").fadeOut("5000",function(){    
+                    
+                        })
+                        
+                    $("#formularioCompra").append(`
+                    <form class="row g-3" style="margin: 5% 0% 0% 30% ;width:30%;  background-color: rgb(15, 15, 15); border: solid 1px white;color:white; ">
+                    <div class="col-md-6">
+                      <label for="inputNombre" class="form-label">Nombre</label>
+                      <input type="text" class="form-control" id="inputNombre" placeholder="Ingrese su nombre">
+                    </div>
+                    <div class="col-md-6">
+                      <label for="inputApellido" class="form-label">Apellido</label>
+                      <input type="text" class="form-control" id="inputApellido" placeholder="Ingrese su apellido">
+                    </div>
+                    <div class="col-12">
+                      <label for="inputDireccion" class="form-label">Direccion</label>
+                      <input type="text" class="form-control" id="inputDireccion" placeholder="Ingrese su direccion" >
+                    </div>
+                    <div class="col-12">
+                      <label for="inputTelefono" class="form-label">Telefono</label>
+                      <input type="text" class="form-control" id="inputTelefono" placeholder="Ingrese su telefono" >
+                    </div>
+                    <div class="col-md-6">
+                      <label for="inputCiudad" class="form-label">Ciudad</label>
+                      <input type="text" class="form-control" id="inputCiudad" placeholder="Ingrese su ciudad">
+                    </div>
+                    <div class="col-md-4">
+                      <label for="inputProvincia" class="form-label">Provincia</label>
+                      <select id="inputProvincia" class="form-select">
+                        <option selected>Elegir...</option>
+                        <option>Buenos Aires</option>
+                        <option>Cordoba</option>
+                        <option>Mendoza</option>
+                        <option>Neuquen</option>
+                        <option>Rio Negro</option>
+                        <option>Santa Cruz</option>
+                        <option>Misiones</option>
+                        <option>Entre Rios</option>
+                        <option>Misiones</option>
+                      </select>
+                    </div>
+                    <div class="col-md-2">
+                      <label for="inputCodigo" class="form-label">Cod. Postal</label>
+                      <input type="text" class="form-control" id="inputCodigo" placeholder="C.P.">
+                    </div>
+            
+                    <div class="col-12">
+                      <button type="submit" class="btn btn-primary" style="margin:  10% 0px 10% 40%; background-color:yellow; color:black;">Enviar</button>
+                    </div>
+                  </form>`)    
+                                                                           
+                    });  
+                
+
+                  });
